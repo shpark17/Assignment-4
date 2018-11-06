f45a15a26a2ea3b41bff7b7c7835bfcaf7ffeb53
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-11-05 19:36:23 -0600

    Create string_utils.c

---
+++ string_utils.c
@@ -0,0 +1,12 @@
+//Author: SungHa Park
+//Date: 11/05/2018
+//This program contains all function prototypes with documentation and their definitions.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+#include"string_utils.h"
+
+int strContains(const char *str, const char *subStr) {
